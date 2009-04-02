f9ead97ed6b62c54f8ffd1a8811018cfca34efad
schonira <ckponnappa@gmail.com>
2009-04-03 00:53:34 +0530

    Added params support for get

--- spec/wrest/uri_spec.rb
+++ spec/wrest/uri_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 module Wrest
-  describe Uri do  
+  describe Uri do
     it "should respond to the four http actions" do
       uri = Uri.new('http://localhost')
       uri.should respond_to(:get)
@@ -9,25 +9,37 @@ module Wrest
       uri.should respond_to(:put)
       uri.should respond_to(:delete)
     end
-  
+
     it "should know when it is https" do
       Uri.new('https://localhost:3000').should be_https
     end
-    
+
     it "should know when it is not https" do
       Uri.new('http://localhost:3000').should_not be_https
     end
-    
+
     it "should know how to get" do
       uri = "http://localhost:3000/glassware".to_uri
       uri.should_not be_https
-      
+
       http = mock(Net::HTTP)
       Net::HTTP.should_receive(:new).with('localhost', 3000).and_return(http)
 
       http.should_receive(:get).with('/glassware?owner=Kai&type=bottle', 'page' => '2', 'per_page' => '5')
-      
+
       uri.get({:owner => 'Kai', :type => 'bottle'}, :page => '2', :per_page => '5')
+    end
+
+    it "should know how to get without any headers" do
+      uri = "http://localhost:3000/glassware".to_uri
+      uri.should_not be_https
+
+      http = mock(Net::HTTP)
+      Net::HTTP.should_receive(:new).with('localhost', 3000).and_return(http)
+
+      http.should_receive(:get).with('/glassware?owner=Kai&type=bottle', {})
+
+      uri.get(:owner => 'Kai', :type => 'bottle')
     end
   end
 end
