1434d1c2166cecaf1e3a6fb067d5e2bec59fb3ce
schonira <ckponnappa@gmail.com>
2009-08-18 19:41:03 +0530

    Fixed the twitter.rb example to be more Object Oriented

--- examples/twitter.rb
+++ examples/twitter.rb
@@ -13,6 +13,12 @@ require 'pp'
 Wrest.logger = Logger.new(STDOUT)
 Wrest.logger.level = Logger::DEBUG  # Set this to Logger::INFO or higher to disable request logging
 
+# This example shows a more object oriented approach
+# to accessing Twitter using Wrest.
+# 
+# Twitter is your twitter account. Every tweet is wrapped in
+# an instance of Tweet. Every Tweet has one TwitterUser.
+
 class Twitter  
   def initialize(options)
     @uri = "https://twitter.com".to_uri(options)
@@ -21,16 +27,55 @@ class Twitter
   # which can be :friends, :user or :public
   # options[:query] can be things like since, since_id, count, etc.
   def timeline(which = :friends, options={})
-    @uri["/statuses/#{which}_timeline.json"].get(options)
+    @uri["/statuses/#{which}_timeline.json"].get(options).deserialise.collect{|tweet| Tweet.new(tweet)}
   end
   
   def post(text)
-    @uri['/statuses/update.json'].post('', {'User-Agent' => "Wrest/#{Wrest::VERSION::STRING}"}, {:status => text})
-  end
+    Tweet.new @uri['/statuses/update.json'].post('', {'User-Agent' => "Wrest/#{Wrest::VERSION::STRING}"}, {:status => text}).deserialise
+  end  
 end
+
+class TwitterUser
+  # This will turn this class into a wrapper
+  # for the desiralised data from a response.
+  #
+  # All the keys in the hash are exposed as methods.
+  include Wrest::Components::AttributesContainer
+
+  # We'd prefer the user's profile url to be
+  # a Wrest::Uri rather than a String, wouldn't we?
+  #
+  # Remember, enabling typecasting support _will_
+  # slow down instance construction marginally, so turn it on
+  # only if you need it.
+  enable_typecasting_support
+
+  typecast :url => lambda{|url| url.to_uri}
+end
+
+class Tweet
+  include Wrest::Components::AttributesContainer
+  enable_typecasting_support
+
+  typecast  :user => lambda{|user| TwitterUser.new(user) }
+end
+
 
 twitter = Twitter.new(:username => 'ponnappa', :password => 'ha!likeImchecking*that*in')
 
-# pp twitter.timeline.deserialise
-# pp twitter.timeline(:friends, :since_id => 20751449).deserialise
-pp twitter.post("This tweet via the Twitter example in #wrest #{Wrest::VERSION::STRING}, http://github.com/kaiwren/wrest").deserialise
+pp twitter.post("This tweet via the Twitter example in #wrest #{Wrest::VERSION::STRING}, http://github.com/kaiwren/wrest")
+
+puts '', '*'*70, ''
+
+tweets = twitter.timeline(:friends, :since_id => 20751449)
+
+# Print the name of the first user in my timeline.
+puts tweets.first.user.name
+
+puts '', '*'*70, ''
+
+# Just remember that not everyone on Twitter has a
+# url. On the other hand, some have more than one.
+# This is just a cute little example that deals with
+# the simple case of a single url.
+puts tweets.first.user.url.get.body
