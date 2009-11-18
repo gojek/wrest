d55171715161b468025212d5f3914a6c063112aa
schonira <ckponnappa@gmail.com>
2009-11-18 10:13:31 +0530

    Bah, the danger of functionals - you skip those unit specs...

--- spec/unit/wrest/http/session_spec.rb
+++ spec/unit/wrest/http/session_spec.rb
@@ -27,5 +27,36 @@ module Wrest
         session.get '/bottles.xml'
       end
     end
+    
+    it "should destroy the current connection if a response is returned with a Connection: Close" do
+      uri = "http://localhost:3000".to_uri
+      uri.should_not be_https
+
+      http = mock(Net::HTTP)
+      Net::HTTP.should_receive(:new).with('localhost', 3000).and_return(http)
+      http.should_receive(:read_timeout=).with(60)
+
+      request_one = Net::HTTP::Get.new('/glassware?owner=Kai&type=bottle', {"Connection"=>"Keep-Alive"})
+      request_two = Net::HTTP::Get.new('/bottles.xml', {"Connection"=>"Keep-Alive"})
+
+      Net::HTTP::Get.should_receive(:new).with('/glassware?owner=Kai&type=bottle', {"Connection"=>"Keep-Alive"}).and_return(request_one)
+      Net::HTTP::Get.should_receive(:new).with('/bottles.xml', {"Connection"=>"Keep-Alive"}).and_return(request_two)
+
+      ok_response = build_ok_response
+      ok_response.should_receive(:[]).with(Http::StandardHeaders::Connection).once.and_return(Http::StandardTokens::KeepAlive)
+
+      ok_response_with_connection_close = build_ok_response
+      ok_response_with_connection_close.should_receive(:[]).with(Http::StandardHeaders::Connection).once.and_return(Http::StandardTokens::Close)
+      
+      http.should_receive(:request).with(request_one, nil).and_return(ok_response)
+      http.should_receive(:request).with(request_two, nil).and_return(ok_response_with_connection_close)
+
+      Http::Session.new(uri) do |session|
+        session.get('/glassware', :owner => 'Kai', :type => 'bottle')
+        session.instance_variable_get('@connection').should == http
+        session.get '/bottles.xml'
+        session.instance_variable_get('@connection').should be_nil
+      end
+    end
   end
 end
