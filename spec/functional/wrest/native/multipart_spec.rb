7ecf130e76b370f19e14b6ad0328ef8a22cfa5a9
aakash <aakashd@gmail.com>
2010-06-26 20:34:27 +0800

    fixing functional spec to use native

--- spec/functional/wrest/native/multipart_spec.rb
+++ spec/functional/wrest/native/multipart_spec.rb
@@ -1,10 +1,14 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require "#{Wrest::Root}/wrest/native"
 require "#{Wrest::Root}/wrest/multipart"
-Wrest::Http = Wrest::Native
 
 module Wrest
   describe Native::PostMultipart do
+    
+    before :all do
+      Wrest.use_native
+    end
+
     it "should know how to post files using multipart" do
       response = nil
       File.open(File.expand_path(__FILE__)) do |file|
