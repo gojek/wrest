92ac8f8dc1ea65152bdec215120e63e565752740
Niranjan Paranjape <niranjan@c42.in>
2010-08-19 10:47:01 +0530

    removing old functional specs

--- spec/functional/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/../../lib/wrest")
-require 'rspec'
-
-Wrest.logger = Logger.new(File.open("#{Wrest::Root}/../log/test.log", 'a'))
-
-def ph(*args)
-  puts *(["<pre>"] + args + ["</pre>"])
-end
