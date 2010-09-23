9b4c0fec36ca385474664a0ef25340985c71ab3d
preethiramdev <preethiramdev@yahoo.com>
2010-09-23 16:32:42 +0530

    XPath filtering support for REXML.Mixes in a module with XmlMini_REXML

--- spec/wrest/xml_mini/rexml_spec.rb
+++ spec/wrest/xml_mini/rexml_spec.rb
@@ -1,12 +1,16 @@
 require "spec_helper"
 
 module ActiveSupport 
-describe XmlMini_REXML, 'filter' do
-  it "should filter using the given xpath and return the first matching node found" do
-    http_response = mock('Http Response')
-    http_response.should_receive(:body).and_return("<Person><Personal><Name><FirstName>ooga</FirstName></Name></Personal><Address><Name>Bangalore</Name></Address></Person>")
+  describe XmlMini_REXML, 'filter' do
+    #  before :each do
+    #   @test_obj = Object.new
+    #   @test_obj.extend(XmlMini_REXML)
+    # end
+      it "should filter using the given xpath and return the first matching node found" do
+      http_response = mock('Http Response')
+      http_response.should_receive(:body).and_return("<Person><Personal><Name><FirstName>ooga</FirstName></Name></Personal><Address><Name>Bangalore</Name></Address></Person>")
 
-    XmlMini_REXML.filter(http_response,'//Name').should == "<Name><FirstName>ooga</FirstName></Name>" 
+      XmlMini_REXML.filter(http_response,'//Name').should == "<Name><FirstName>ooga</FirstName></Name>" 
+    end
   end
-end
 end
