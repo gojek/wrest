5e4dc54b0e7f15b820ac1062b09776e8f758beb7
preethiramdev <preethiramdev@yahoo.com>
2010-09-29 15:45:53 +0530

    Updated changelog and gemspec for beta7 release

--- lib/wrest/version.rb
+++ lib/wrest/version.rb
@@ -13,7 +13,7 @@ module Wrest
       MAJOR  = 1
       MINOR  = 0
       TINY   = 0
-      BUILD = 'beta6'
+      BUILD = 'beta7'
       
       STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
 
