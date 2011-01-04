f13f8af9d19c88993ee90f516f2a532fe9e7363c
Jasim A Basheer <jasim@c42.in>
2011-01-04 16:11:24 +0530

    version bump 1.0.1

--- lib/wrest/version.rb
+++ lib/wrest/version.rb
@@ -12,7 +12,7 @@ module Wrest
     unless defined? MAJOR
       MAJOR  = 1
       MINOR  = 0
-      TINY   = 0
+      TINY   = 1
       BUILD = nil
       
       STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
