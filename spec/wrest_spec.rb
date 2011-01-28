2f4f11194e9db9691eb0d6934f1d7b74b12206b9
Sidu Ponnappa <ckponnappa@gmail.com>
2011-01-28 15:37:44 +0530

    Minor changes to spec

--- spec/wrest_spec.rb
+++ spec/wrest_spec.rb
@@ -6,7 +6,7 @@ describe Wrest do
 
     it "should use memached when always_cache_using_memcached is called" do
       Wrest.always_cache_using_memcached!
-      Wrest.default_cachestore.is_a?(Wrest::Components::CacheStore::Memcached).should be_true
+      Wrest.default_cachestore.should be_a(Wrest::Components::CacheStore::Memcached)
 
       Wrest.default_cachestore.should_receive(:[]=)
       "http://localhost:3000/cacheable/cant_be_validated/with_expires/300".to_uri.get
@@ -14,11 +14,10 @@ describe Wrest do
 
     it "should use a Hash when always_cache_using_hash is called" do
       Wrest.always_cache_using_hash!
-      Wrest.default_cachestore.is_a?(Hash).should be_true
+      Wrest.default_cachestore.should be_a(Hash)
 
       Wrest.default_cachestore.should_receive(:[]=)
       "http://localhost:3000/cacheable/cant_be_validated/with_expires/300".to_uri.get
     end
-
   end
 end
