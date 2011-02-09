d20aa7da9cf669c3b999a0d4065718c9cb0fec69
Arvind Laxminarayan <arvind@c42.in>
2011-02-09 20:35:07 +0530

    Threading functionality extracted into a class

---
+++ spec/wrest/thread_backend_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+module Wrest
+  describe Wrest::ThreadBackend do
+
+    describe "executing requests" do
+      it "should execute the given request asynchronously using threads" do
+        hash = {}
+        uri = "http://localhost:3000".to_uri
+        request = Wrest::Native::Get.new(uri,{},{},:callback => Callback.new({200 => lambda{|response| hash["success"] = true}}))
+        response_200 = mock(Net::HTTPOK, :code => "200", :message => 'OK', :body => '', :to_hash => {})
+        request.should_receive(:do_request).and_return(response_200)
+
+        async_obj = Wrest::ThreadBackend.new
+        async_obj.execute(request)
+        sleep 1
+        hash.key?("success").should be_true
+      end
+    end
+  end
+end
