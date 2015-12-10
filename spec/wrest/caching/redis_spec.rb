a6c02ad21e0912f7f204768851f7247c43539d15
vjdhama <vjdhama26@gmail.com>
2015-12-11 00:05:59 +0530

    Refactored redis specs. - Added *.rdb files to .gitignore

--- spec/wrest/caching/redis_spec.rb
+++ spec/wrest/caching/redis_spec.rb
@@ -5,9 +5,7 @@ Wrest::Caching.enable_redis
 
 describe Wrest::Caching do
   context "functional", :functional => true do
-    before :each do
-      @redis = Wrest::Caching::Redis.new
-    end
+    let(:redis) { Wrest::Caching::Redis.new }
 
     context "initialization defaults" do
       it "should always default the options to an empty hash" do
@@ -24,33 +22,33 @@ describe Wrest::Caching do
     it "should know how to retrieve a cache entry" do
       request = 'http://localhost:3000/cacheable/can_be_validated/with_last_modified/always_304/1000'
       ok_response = request.to_uri.get
-      @redis[request] = ok_response
-      expect(@redis[request]).to eq(ok_response)
+      redis[request] = ok_response
+      expect(redis[request]).to eq(ok_response)
     end
 
     it 'should unmarshall the value when retrieved a cache entry' do
       ok_response = 'http://localhost:3000/cacheable/can_be_validated/with_last_modified/always_304/1000'.to_uri.get
-      @redis['example-123'] = ok_response
+      redis['example-123'] = ok_response
 
-      expect(@redis['example-123']).to eq(ok_response)
+      expect(redis['example-123']).to eq(ok_response)
     end
 
     it "should set expiry for the cache entry based on response headers" do
       uri_string = 'http://localhost:3000/cacheable/can_be_validated/with_last_modified/always_give_fresh_response/10'
       ok_response = uri_string.to_uri.get
-      @redis[uri_string] = ok_response
-      expect(@redis.instance_eval('@redis').ttl(uri_string)).to eq(10)
+      redis[uri_string] = ok_response
+      expect(redis.instance_eval('@redis').ttl(uri_string)).to eq(10)
     end
     
     it "should know how to delete a cache entry" do
       request = 'http://localhost:3000/cacheable/can_be_validated/with_last_modified/always_304/1000'
       ok_response = request.to_uri.get
-      @redis[request] = ok_response
-      @redis.delete(request).should == ok_response
+      redis[request] = ok_response
+      redis.delete(request).should == ok_response
     end
 
     it 'should return nil on delete if the key is not present' do
-      expect(@redis.delete('not_present_key')).to eq(nil)
+      expect(redis.delete('not_present_key')).to eq(nil)
     end
   end
 end
