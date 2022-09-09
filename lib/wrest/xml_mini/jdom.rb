3adb4d2c751de13b197b90cf3e86efed8ebc8908
Sidu Ponnappa <ckponnappa@gmail.com>
2022-09-10 03:18:35 +0800

    - Fix jruby build by patching in Jruby 9.3.x.x fix to XmlMini JDom from Rails 7 as a local copy

--- lib/wrest/xml_mini/jdom.rb
+++ lib/wrest/xml_mini/jdom.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-require 'wrest/xml_mini/jdom/xpath_filter'
+require File.join('active_support', 'xml_mini', 'jdom')
+require File.join('wrest', 'xml_mini', 'jdom', 'xpath_filter')
+
 module ActiveSupport
   module XmlMini_JDOM
     XmlMini_JDOM.extend(::XmlMini::JDOM::XPathFilter)
